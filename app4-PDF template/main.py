e67633c005549c0aaaf9a7f0a711e1b02c522088
georgeknight <georgeknightprogramming@gmail.com>
2023-04-04 12:55:46 -0500

    Add footer

--- app4-PDF template/main.py
+++ app4-PDF template/main.py
@@ -2,17 +2,32 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF (orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=24, txt=row["Topic"], align="L", ln=1)
     pdf.line(x1=10, y1=30, x2=200, y2=30)
 
+    pdf.ln(250)
+
+#Set the footer for master page
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+
+        pdf.ln(274)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
 pdf.output("output.pdf")
