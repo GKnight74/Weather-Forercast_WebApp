966cbafc640bab63003901187cb4a7567ee772e4
georgeknight <georgeknightprogramming@gmail.com>
2023-04-08 05:54:53 -0500

    Add send_email function

--- Email_API/main.py
+++ Email_API/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "bfd5bf377c5f4f92b137814564f34662"
 url = "https://newsapi.org/v2/everything?q=apple&from=2023-04-07&" \
@@ -12,5 +13,10 @@ request = requests.get(url)
 content = request.json()
 
 # Acces the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
+    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+# Send email
+body = body.encode("utf-8")
+send_email(message=body)
